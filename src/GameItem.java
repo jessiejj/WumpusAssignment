01f6e0837692b929553c34a88454311920002588
calvin <s3627341@student.rmit.edu.au>
2016-10-03 20:51:24 +1100

    minor change to GameItem

--- src/GameItem.java
+++ src/GameItem.java
@@ -3,15 +3,11 @@ public abstract class GameItem {
 public GameItem gameItem;
 	
 	char c;
-//	int row;
-//	int col;
-	
 
 	public GameItem(char c) {
 		super();
 		this.c = c;
 	}
-
 
 	public abstract void display();
 }
